08d90140e1f80f34d86b9e6c612e903e2c5fb3c9
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-01-30 16:39:35 -0500

    api data fetch

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import countriesData from '../countriesData';
+import React, { useEffect, useState } from 'react';
+// import countriesData from '../countriesData';
 import CountryCard from './CountryCard';
 
-export default function CountriesList({query}) 
+export default function CountriesList({query}) {
 
-{
+const [countriesData, setCountriesData] = useState([])
+
+
+useEffect(()=>{
+
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json())
+  .then(data => {
+   setCountriesData(data)
+  })
+}, [])
+
+
 const array= countriesData.filter((country) =>
 country.name.common.toLowerCase().includes(query)).map((country)=>{
-  console.log(country)
   return <CountryCard key={country.name.common} name={country.name.common} capital={country.capital} 
   flag={country.flags.svg} population={country.population} region={country.region}/>
 })
