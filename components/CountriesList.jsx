c7008a45adf27a58063f7f6255c2704badcca908
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-01-30 00:27:46 -0500

    minor fix, data import from countriesData.js

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -7,7 +7,7 @@ export default function CountriesList()
 {
 const array= countriesData.map((country)=>{
   console.log(country)
-  return <CountryCard name={country.name.common} capital={country.capital} 
+  return <CountryCard key={country.name.common} name={country.name.common} capital={country.capital} 
   flag={country.flags.svg} population={country.population} region={country.region}/>
 })
 
--- components/CountryCard.jsx
+++ components/CountryCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function CountryCard({key, name,capital,flag,population,region}) {
+export default function CountryCard({name,capital,flag,population,region}) {
   return (
-    <a href="/country.html?name={}name" className="country-card" key={key}>
-        <img src={flag} alt="{name} flag" />
+    <a href={`/country.html?name=${name}`}  className="country-card">
+        <img src={flag} alt={name + " flag"}  />
         <div className="card-text">
             <h3 className='card-title'>{name}</h3>                        
             <p><b>Population:{population} </b> </p>
