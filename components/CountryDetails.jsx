db33b4faa770b64cd51254bfa44c3b8dcb6783ec
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-01-31 16:05:12 -0500

    multiple fetch for borders working

--- components/CountryDetails.jsx
+++ components/CountryDetails.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./CountryDetails.css";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 
 export default function CountryDetails() {
-//   const countryName = new URLSearchParams(location.search).get("name");
-    const params = useParams();
-    const countryName = params.country;
-    
+  const params = useParams();
+  const countryName = params.country;
 
   const [countryData, setCountryData] = useState(null);
   const [notFound, setNotFound] = useState(false);
@@ -15,35 +13,56 @@ export default function CountryDetails() {
     fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
       .then((res) => res.json())
       .then(([data]) => {
-          setCountryData({
-            country: data.name.common,
-            flag: data.flags.svg,
-            nativeName: Object.values(data.name.nativeName)[0].common,
-            population: data.population,
-            region: data.region,
-            subregion: data.subregion,
-            capital: data.capital,
-            tld: data.tld,
-            currencies: Object.values(data.currencies)
-              .map((currency) => currency.name)
-              .join(", "),
-            languages: Object.values(data.languages).join(" ,"),
-            // borders: Object.values(data.borders).map((border)=> border).join(', ')
-          });
-      }).catch(err =>{
+        setCountryData({
+          country: data.name.common,
+          flag: data.flags.svg,
+          nativeName: Object.values(data.name.nativeName)[0].common,
+          population: data.population,
+          region: data.region,
+          subregion: data.subregion,
+          capital: data.capital,
+          tld: data.tld,
+          currencies: Object.values(data.currencies)
+            .map((currency) => currency.name)
+            .join(", "),
+          languages: Object.values(data.languages).join(" ,"),
+          borders: []
+        })
+
+        if(!data.borders){
+            data.borders = []
+        }
+
+        Promise.all(data.borders.map((border) =>{
+            return fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+            .then((res) => res.json())
+            .then(([borderCountry]) =>{
+                return borderCountry.name.common
+                // setCountryData((prev) => ({ ...prev, borders: [...prev.borders, borderCountry.name.common ] }))
+            })
+        })).then((borders) =>{
+            setCountryData((prev) => ({...prev, borders}))
+        })
+      })
+      .catch((err) => {
         setNotFound(true);
       });
-  }, []);
+  }, [countryName]);
 
-  if(notFound){
-    return <div>Page you want to access is not valid</div>
+  if (notFound) {
+    return <div>Page you want to access is not valid</div>;
   }
   return countryData === null ? (
     "Loading...."
   ) : (
     <main>
       <div className="country-details-container">
-        <span className="back-button" onClick={(()=>{history.back()})}>
+        <span
+          className="back-button"
+          onClick={() => {
+            history.back();
+          }}
+        >
           <i className="fa-solid fa-arrow-left"></i>&nbsp; Back
         </span>
         <div className="country-details">
@@ -84,9 +103,14 @@ export default function CountryDetails() {
                 <span className="language"> </span>
               </p>
             </div>
-            <div className="border-countries">
-              <b>Border Countries: {countryData.borders} </b>&nbsp;
-            </div>
+            {countryData.borders.length !== 0 && (
+              <div className="border-countries">
+                <b>Border Countries: </b>&nbsp;
+                {countryData.borders.map((border) => (
+                  <Link key={border} to={`/${border}`}>{border}</Link>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
