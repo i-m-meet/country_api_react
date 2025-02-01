a8990fdcbe8f1cac939dff5cc05b607fe4992684
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-01 13:40:42 -0500

    dark mode works

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header() {
+
+  const [isDark, setIsDark] = useState(false)
+
   return (
     <header className="header-container">
       <div className="header-content">
         <h2 className="title">
           <a href="/">Where in the world?</a>
         </h2>
-        <p className="theme-changer">
-          <i className="fa-regular fa-moon"></i>&nbsp;&nbsp;Dark Mode
+        <p className="theme-changer" onClick={() =>{
+          document.body.classList.toggle('dark');
+          setIsDark(!isDark)
+        }}>
+          <i className={`fa-solid fa-${isDark? "sun" : "moon"}`}></i>&nbsp;&nbsp;{`${isDark? "Light" : "Dark"} mode`}
         </p>
       </div>
     </header>
