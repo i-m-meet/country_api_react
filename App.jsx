01988823e963c85832de5643c8584501cfd1bb78
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-01 14:50:35 -0500

    dark mode works perfectly

--- App.jsx
+++ App.jsx
@@ -1,13 +1,16 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
 import { Outlet } from "react-router";
 
 const App = () => {
 
+    const [isDark, setIsDark] = useState(JSON.parse(localStorage.getItem('isDarkMode')));
+
     
     return(
         <>
-        <Header/>
-        <Outlet/>
+        <Header theme={[isDark, setIsDark]}/>
+        <Outlet context={[isDark, setIsDark]}/>
         
         </>
     );
